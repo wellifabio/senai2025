613c54576f17ac5e6e3a742537b3443e09a26a45
wellifabio <wellifabio@hotmail.com>
2025-08-09 10:33:01 -0300

    office

---
+++ ds/4des/04-ts/aula01/pedidosapi/prisma/migrations/20250806182224_init/migration.sql
@@ -0,0 +1,39 @@
+-- CreateTable
+CREATE TABLE `Usuario` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `nome` VARCHAR(191) NOT NULL,
+    `senha` VARCHAR(191) NOT NULL,
+    `tipo` ENUM('ADMIN', 'CLIENTE') NOT NULL,
+
+    UNIQUE INDEX `Usuario_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Produto` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `nome` VARCHAR(191) NOT NULL,
+    `descricao` VARCHAR(191) NOT NULL,
+    `imagem` VARCHAR(191) NOT NULL,
+    `preco` DOUBLE NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Pedido` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `usuarioId` INTEGER NOT NULL,
+    `produtoId` INTEGER NOT NULL,
+    `quantidade` INTEGER NOT NULL,
+    `data` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Pedido` ADD CONSTRAINT `Pedido_usuarioId_fkey` FOREIGN KEY (`usuarioId`) REFERENCES `Usuario`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Pedido` ADD CONSTRAINT `Pedido_produtoId_fkey` FOREIGN KEY (`produtoId`) REFERENCES `Produto`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ office/README.md
@@ -0,0 +1,8 @@
+# MICROSOFT OFFICE SPECIALIST ASSOCIATE
+
+|UNIDADE CURRICULAR|CARGA HORÁRIA |
+|-|-|
+|MO-100 (Microsoft Office Word)|20h|
+|MO-200 (Microsoft Office Excel)|20h|
+|MO-300 (Microsoft Office Powerpoint)|20h|
+|Carga horária total|60h|
